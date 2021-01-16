6c8cc0033e7b8c6482ef00a918e85a946b2cb8a6
ozovadia94 <ozovad@gmail.com>
2021-01-16 10:20:39 +0200

    users

--- src/User/Users.js
+++ src/User/Users.js
@@ -10,18 +10,20 @@ const user = (props) => (
             <td>{props.gituser}</td>
             <td>{props.gitproject}</td>
             <td>{props.jira}</td>
-            <td><a href="#home" onClick={props.clicked2} class="btn btn-outline-warning buttLink Logged-out" data-toggle="modal" data-target="#modalLRForm">חלון התקדמות בגיט</a></td>
-            
-          
+            <td><a href="#home" class="btn btn-outline-warning buttLink Logged-out" data-toggle="modal" data-target="#modalLRForm">חלון התקדמות בגיט</a>
+
+                  <Student
+                        gituser={props.gituser}
+                        gitproject={props.gitproject}
+                  />
+            </td>
+
+
 
             <td><button onClick={props.clicked} type="button" class="btn btn-danger btn-sm">מחק</button> </td>
       </tr>
 
 
 );
-
-
-
-
 export default user;
 
