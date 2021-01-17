42a8b564cb4141fdfcd20a933103c3ff2ee8089b
ozovadia94 <ozovad@gmail.com>
2021-01-17 20:18:50 +0200

    17/1/21 commits dash

--- src/User/‏‏Students.js
+++ src/User/‏‏Students.js
@@ -3,7 +3,13 @@ import MyTitle from '../Titles/Title'
 import axios from 'axios';
 import './Add_User.css' /* CSS */
 
-const moment = require('moment');
+//import urlParams from 'url-search-params'
+
+var URLSearchParams = require('url-search-params');
+
+//const moment = require('moment');
+
+
 
 
 class git extends Component {
@@ -21,92 +27,117 @@ class git extends Component {
 
 
     componentDidMount() {
-        var st = this.return_address(this.gituser, this.gitproject)
+        this.create_Users().then(res => {
+            console.log('HERE1')
+            console.log(res)
+            console.log('HERE2')
+            return true
+        }).then(res => {
+            this.setState({ loading: true });
+            console.log('HERE3')
+        }).catch(err => {
+            this.setState({ loading: false });
+        })
+    }
+
+    create_Users = async () => {
+        var st = this.return_address()
         const fetchedUsers = [];
-        axios.get(st)
-            .then(res => {
-                for (let key in res.data) {
-                    fetchedUsers.push({
-                        ...res.data[key],
-                        id: key
-                    });
-                }
-            }).then(res => {
-                for (let key in fetchedUsers) {
-                    axios.get(st + '/' + fetchedUsers[key].sha)
-                        .then(res2 => {
-                            fetchedUsers[key].stats = {
-                                total: res2.data.stats.total,
-                                additions: res2.data.stats.additions,
-                                deletions: res2.data.stats.deletions,
-                            };
-                        })
-                }
-            }).then(res => {
-                this.setState({ loading: true, users: fetchedUsers });
-            })
-            .catch(err => {
-                this.setState({ loading: false });
-            })
 
+        let promise = new Promise((res) => {
+            setTimeout(() => res("Now it's done!"), 2000)
+            axios.get(st)
+                .then(res => {
+                    for (let key in res.data) {
+                        fetchedUsers.push({
+                            ...res.data[key],
+                            id: key,
+                        });
+
+                    }
+                    return fetchedUsers
+                }).then(res => {
+                    console.log('size:' + res.length)
+                    for (let key = 0; key < res.length; key++) {
+                        console.log('asd')
+                        axios.get(st + '/' + res[key].sha)
+                            .then(res2 => {
+                                fetchedUsers[key]['additions']=res2.data.stats.additions
+                                fetchedUsers[key]['deletions']=res2.data.stats.deletions
+                                fetchedUsers[key]['total']=res2.data.stats.total
+                            })
+
+                    }
+                })
+        });
+        // wait until the promise returns us a value
+        let result = await promise;
+
+        // "Now it's done!"
+        this.sleep(2000)
+        this.setState({users: fetchedUsers, });
+
+        return fetchedUsers
     }
 
-    return_address(user, repos) {
-        return 'https://api.github.com/repos/' + user + '/' + repos + '/commits'
-    }
+ 
 
-    render() {
+sleep(milliseconds) {
+    const date = Date.now();
+    let currentDate = null;
+    do {
+        currentDate = Date.now();
+    } while (currentDate - date < milliseconds);
+}
 
-        return (
+return_address() {
+    const queryString = window.location.search;
+    var params2 = new URLSearchParams(queryString);
+    var user = params2.get('gituser')
+    var repos = params2.get('gitproject')
+    return 'https://api.github.com/repos/' + user + '/' + repos + '/commits'
+}
 
-            <div className="col-md-6">
-                <form>
-                    <div class="modal fade" id="modalLRForm" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-                        <div class="modal-dialog cascading-modal" role="document">
-                            <div class="modal-content">
-                                <MyTitle title="לוח התקדמות" />
+render() {
 
-                                {this.state.loading ? (
-                                    <div>
-                                        <table class="table">
-                                            <tr>
-                                                <th>#</th>
-                                                <th>Date1 </th>
-                                                <th>Date2 </th>
-                                                <th>Name</th>
-                                                <th>Changes</th>
-                                            </tr>
+    return (
 
-                                            {console.log(this.state.users)}
-                                            {console.log(this.state.users[2])}
-                                            {console.log(this.state.users[2].stats)}
-                                            {this.state.users.map(user => (
-                                                <tr>
-                                                    <th>{user.id}</th>
-                                                    <th>{moment(user.commit.author.date).format("DD/MM/YYYY HH:MM:SS")}</th>
-                                                    <th>{user.commit.author.date}</th>
-                                                    <th>{user.commit.message}</th>
-                                                    <th>{user.stats}</th>
-                                                    <th>{user.stats}</th>
-                                                    <th>{user.stats}</th>
-                                                </tr>
-                                            ))}
+        <div className="col-md-6">
 
-                                        </table>
-                                    </div>
-                                ) : (<div>  </div>)}
+            <MyTitle title="לוח התקדמות" />
 
-                            </div>
-                        </div>
-                    </div>
-                </form>
-            </div>
+            {this.state.loading ? (
+                <div>
+                    <table class="table">
+                        <tr>
+                            <th>#</th>
+                            <th>Date</th>
+                            <th>Name</th>
+                            <th>Changes</th>
+                        </tr>
+
+                        {this.state.users.map(user => (
+                            <tr>
+                                <th>{parseInt(user.id) + 1}</th>
+                                <th>{user.commit.author.date}</th>
+                                <th>{user.commit.message}</th>
+                                <th>{user.total}</th>
+                            </tr>
+                        ))}
 
 
+                    </table>
+                </div>
+            ) : (<div>  </div>)}
 
 
-        );
-    }
+        </div>
+
+
+
+
+    );
+}
 
 }
 
