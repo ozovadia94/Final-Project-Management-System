336a294077675301d76cc6dca34774c482a6c285
ozovadia94 <ozovad@gmail.com>
2021-01-27 02:03:06 +0200

    27/1/21 create special alerts

---
+++ src/JS/Alerts.js
@@ -0,0 +1,48 @@
+import Swal from 'sweetalert2'
+
+var my_success = (titl,if_reload=true) => {
+    Swal.fire({
+        //position: 'top',
+        icon: 'success',
+        title: titl,
+        showConfirmButton: false,
+        timer: 1000
+    }).then(()=>{
+        if(if_reload)
+            window.location.reload()
+    })
+}
+
+var are_you_sure = async (text,id,func) => {
+    Swal.fire({
+        title: 'שים לב',
+        text: text,
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'כן',
+        cancelButtonText: 'לא'
+        
+      }).then((result) => {
+        if (result.isConfirmed) 
+            func(id)
+      })
+}
+
+var my_error = (props) => {
+    console.log(props)
+
+    Swal.fire({
+        icon: 'error',
+        title: 'שגיאה!',
+        text: props,
+    })
+
+}
+
+export default {
+    my_success,
+    my_error,
+    are_you_sure,
+};
